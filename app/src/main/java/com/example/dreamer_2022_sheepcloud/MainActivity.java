2b79c26fd819aeca569d5427beaa8b14e55d07b4
gaeul <chlrkdmf0805@naver.com>
2022-08-01 15:12:40 +0900

    홈 깃털 넣기

--- app/src/main/java/com/example/dreamer_2022_sheepcloud/MainActivity.java
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dreamer_2022_sheepcloud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
