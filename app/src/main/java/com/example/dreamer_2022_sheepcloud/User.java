71448fae87d27ee670b803455676faa1b4ef5cad
yday05506 <yangdayeon2@gmail.com>
2022-08-02 15:39:44 +0900

    n월 달의 꿈(글꼴 안 바꿈)

---
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/User.java
@@ -0,0 +1,19 @@
+package com.example.dreamer_2022_sheepcloud;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class User extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+}
