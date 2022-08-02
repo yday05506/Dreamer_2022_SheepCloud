9aec403e6e1a0aa2fb0ccf6ed992c21174a6c919
iamhanori <s2122@e-mirim.hs.kr>
2022-08-02 14:17:09 +0900

    tab_bar

--- app/src/main/java/com/example/dreamer_2022_sheepcloud/MainActivity.java
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView btnPlus = findViewById(R.id.btn_plus);
-        btnPlus.bringToFront();
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
