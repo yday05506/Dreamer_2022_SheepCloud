306fca0362f6ba613918e4cc331bda11f2f4cb6d
iamhanori <s2122@e-mirim.hs.kr>
2022-08-04 15:09:38 +0900

    메뉴바 이미지버튼으로 변경

--- app/src/main/java/com/example/dreamer_2022_sheepcloud/MainActivity.java
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView n_textv; // (1) n월 달의 꿈
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,25 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        n_textv = findViewById(R.id.t_textv);
     }
+
+
+    @Override
+    protected void onStart() {
+        getDT(); // (1) n월
+        super.onStart();
+    }
+
+    
+    // (1) n월
+    public void getDT(){
+        Calendar cal = Calendar.getInstance();
+        int m = 0;
+        m = cal.get(Calendar.MONTH) +1;
+        n_textv.setText(m+"월 달의");
+    }
+
+
 }
