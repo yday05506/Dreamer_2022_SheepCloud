30b17719e75e30c53695639d5b3a62b15f344778
yday05506 <yangdayeon2@gmail.com>
2022-08-05 15:46:34 +0900

    list UI 작성

--- app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
@@ -6,7 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class WriteActivity extends AppCompatActivity {
     ImageButton btnList, btnUser;
@@ -15,6 +19,25 @@ public class WriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+        final String[] cultureKind = {"뮤지컬", "책", "영화", "드라마", "미술/박물관", "기타"};
+
+        Spinner spinner = findViewById(R.id.spinner_kind);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, cultureKind);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         // 타이틀바 없애는 거임!! 지우지 마셈!!
         ActionBar actionBar = getSupportActionBar();
