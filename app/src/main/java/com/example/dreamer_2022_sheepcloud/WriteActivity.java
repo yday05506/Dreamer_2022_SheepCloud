df965bc1a831ccabe4c3b1412503291d18fe1047
gaeul <chlrkdmf0805@gmail.com>
2022-09-13 19:59:42 +0900

    write 수정 중

--- app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
@@ -1,14 +1,45 @@
 package com.example.dreamer_2022_sheepcloud;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class WriteActivity extends AppCompatActivity {
+    TextView textView;
+    String[] cultureKind = {"뮤지컬", "책", "영화", "드라마", "미술관/박물관", "기타"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        textView = findViewById(R.id.write_textView);
+        Spinner spinner = findViewById(R.id.write_spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, cultureKind);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                textView.setText(cultureKind[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                textView.setText("");
+            }
+        });
+
+        // 타이틀바 없애는 거임!! 지우지 마셈!!
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
