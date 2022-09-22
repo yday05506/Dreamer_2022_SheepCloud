49260f0c7c33db35fa7f19ffe82462fca8b7e2a6
gaeul <chlrkdmf0805@gmail.com>
2022-09-22 19:33:02 +0900

    write 틀 완성

--- app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
@@ -5,19 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WriteActivity extends AppCompatActivity {
-    TextView textView;
-    DatePickerDialog datePickerDialog;
+    TextView textView, writeDate;
     String[] cultureKind = {"종류 선택", "뮤지컬", "책", "영화", "드라마", "미술관/박물관", "기타"};
+    ImageButton btnList, btnUser;
+
+    long mNow = System.currentTimeMillis();
+    Date mReDate = new Date(mNow);
+    SimpleDateFormat mFormat = new SimpleDateFormat("MM / dd");
+    String formatDate = mFormat.format(mReDate);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,11 @@ public class WriteActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.write_textView);
         Spinner spinner = findViewById(R.id.write_spinner);
+        writeDate = findViewById(R.id.write_date);
+        writeDate.setText(formatDate);
+
+        btnList = findViewById(R.id.btn_list);
+        btnUser = findViewById(R.id.btn_user);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, cultureKind);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -40,6 +55,22 @@ public class WriteActivity extends AppCompatActivity {
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
                 textView.setText("");
+            }
+        });
+
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                startActivity(intent);
             }
         });
 
