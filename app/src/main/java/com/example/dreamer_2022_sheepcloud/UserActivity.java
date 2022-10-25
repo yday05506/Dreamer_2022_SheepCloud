a48843723d5f78fe74aeb1af99f96299cfc933a4
gaeul <chlrkdmf0805@gmail.com>
2022-10-25 19:33:07 +0900

    프로필 사진 등록?

--- app/src/main/java/com/example/dreamer_2022_sheepcloud/UserActivity.java
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/UserActivity.java
@@ -4,9 +4,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
@@ -19,12 +22,17 @@ import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class UserActivity extends AppCompatActivity {
     private static final Bundle REQUEST_CODE = null;
     ImageButton btnList, btnWrite;
-    ImageView u_img; // 프로필
+    ImageButton u_img; // 프로필
+    String imgName = "user.png";    // 이미지 이름
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,32 +63,61 @@ public class UserActivity extends AppCompatActivity {
             }
         });
 
-//        u_img.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                PopupMenu pop = new PopupMenu(getApplicationContext(), view);
-//
-//                pop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-//                    @Override
-//                    public boolean onMenuItemClick(MenuItem menuItem) {
-//                        switch (menuItem.getItemId()){
-//
-//                        }
-//
-//                        return true;
-//                    }
-//                });
-//            }
-//        });
-
-
+        try {
+            String imgpath = getCacheDir() + "/" + imgName;  // 내부 저장소에 저장되어 있는 이미지 경로
+            Bitmap bm = BitmapFactory.decodeFile(imgpath);
+            u_img.setImageBitmap(bm);   // 내부 저장소에 저장된 이미지를 이미지버튼에 셋
+            Toast.makeText(getApplicationContext(), "파일 로드 성공", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "파일 로드 실패", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    private Bitmap rotateImage(Bitmap source, int angle) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(angle);
-        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    public void bt1(View view) {    // 이미지 고를 갤러리 오픈
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 101);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 101) {
+            if(resultCode == RESULT_OK) {
+                Uri fileUri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                try {
+                    InputStream instream = resolver.openInputStream(fileUri);
+                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
+                    u_img.setImageBitmap(imgBitmap);    // 선택한 이미지 셋
+                    instream.close();   // 스트림 닫아주기
+                    saveBitmapToJpeg(imgBitmap);    // 내부 저장소에 저장
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 성공", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "파일 불러오기 실패", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
+    private void saveBitmapToJpeg(Bitmap imgBitmap) {   // 선택한 이미지 내부 저장소에 저장
+        File tempFile = new File(getCacheDir(), imgName);   // 파일 경로와 이름 넣기
+        try {
+            tempFile.createNewFile();   // 자동으로 빈 파일 생성
+            FileOutputStream out = new FileOutputStream(tempFile);  // 파일을 쓸 수 있는 스트림 준비
+            imgBitmap.compress(Bitmap.CompressFormat.PNG, 100, out);    // compress 함수를 사용해 스트림에 비트맵 넣기
+            out.close();
+            Toast.makeText(getApplicationContext(), "파일 저장 성공", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "파일 저장 실패", Toast.LENGTH_SHORT).show();
+        }
+
+//    private Bitmap rotateImage(Bitmap source, int angle) {
+//        Matrix matrix = new Matrix();
+//        matrix.postRotate(angle);
+//        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+//    }
+
+    }
 }
