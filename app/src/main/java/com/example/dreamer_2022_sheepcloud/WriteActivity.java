4b684ecf15f7dcedb0003685822e37107866bbf0
yday05506 <yangdayeon2@gmail.com>
2022-11-01 19:13:28 +0900

    spinner 커스텀 중

--- app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
@@ -11,6 +11,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
