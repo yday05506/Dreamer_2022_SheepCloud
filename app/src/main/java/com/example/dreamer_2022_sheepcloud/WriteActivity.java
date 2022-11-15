15d03e9c43e700615a0da3951cfca3fffcec74be
iamhanori <s2122@e-mirim.hs.kr>
2022-11-15 18:36:23 +0900

    커밋확인

--- app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
+++ app/src/main/java/com/example/dreamer_2022_sheepcloud/WriteActivity.java
@@ -100,6 +100,7 @@ public class WriteActivity extends AppCompatActivity {
             }
         });
 
+
         btnUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
